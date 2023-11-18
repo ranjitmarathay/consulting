1ae78078b106fcd8cd61e7f4ded0b093599b321a
Ranjit Marathay <rmarathay@gmail.com>
2023-11-17 18:17:25 -0800

    push mobile changes

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -7,18 +7,24 @@ import {
     Toolbar,
     Grid,
     AppBar,
+    Link,
 } from "@mui/material";
+
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+import EmailIcon from '@mui/icons-material/Email';
+import LanguageIcon from '@mui/icons-material/Language';
 
 const TextMeButton = () => {
 
     const phoneNumber = "+14084761238";
     const email = "rmarathay+consulting@gmail.com"
     const emailMessage = "Hello, I would like to chat about a custom data solution!\n\nMy name is:\n\nThe name of my business is:\n\nMy website is:\n\nThe problem I am facing is:\n\n Thanks!\n <Your Name>"
+    const textMessage = "Hi,%I%would%like%to%chat%about%a%custom%data%solution!"
 
     const handleClick = () => {
         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
             // Mobile device detected
-            window.open(`sms:${phoneNumber}?body=${encodeURIComponent(emailMessage)}`);
+            window.open(`sms:${phoneNumber}?body=${encodeURIComponent(textMessage)}`);
         } else {
             // Non-mobile device, prompt to manually send a message
             window.open(`mailto:${email}?body=${encodeURIComponent(emailMessage)}`)
@@ -61,19 +67,33 @@ const TextMeButton = () => {
 }
 
 export default function LandingPage(){
+
+    const paddingLeftVar = "10px"
+
     return (
         <Box
             sx={{
-                width: "70%",
+                width: { xs: "100%", sm: "90%", md: "80%", lg: "70%" }, // Responsive width
                 mx: "auto",
                 paddingTop: "25px"
             }}
         >
             <AppBar position="static" sx={{ bgcolor: 'white', boxShadow: '0 4px 2px -2px gray', borderColor: "#000000" }}>
                 <Toolbar>
-                    <Typography fontSize={34} fontWeight={800} color={"#000000"}>
+                    <Typography fontSize={34} sx={{ flexGrow: 1 }} fontWeight={800} color={"#000000"}>
                         Ranjit Marathay
                     </Typography>
+                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <Link href="tel:+14084761238" color="inherit" sx={{ marginX: 1, display: 'flex', alignItems: 'center' }}>
+                            <SmartphoneIcon sx={{ mr: 0.5, color: "#000000" }} />s
+                        </Link>
+                        <Link href="mailto:rmarathay+consulting@gmail.com" color="inherit" sx={{ marginX: 1, display: 'flex', alignItems: 'center' }}>
+                            <EmailIcon sx={{ mr: 0.5, color: "#000000"}} />
+                        </Link>
+                        <Link href="https://ranjitmarathay.com" color="inherit" sx={{ marginX: 1, display: 'flex', alignItems: 'center' }}>
+                            <LanguageIcon sx={{ mr: 0.5, color: "#000000" }} />
+                        </Link>
+                    </Box>
                 </Toolbar>
             </AppBar>            
             <Grid 
@@ -86,20 +106,6 @@ export default function LandingPage(){
                     paddingTop: "30px"
                 }}
             >
-                {/* <Grid item xs={12}>
-                    <Box
-                        sx={{
-                            textAlign: "flex-start",
-                            paddingBottom: "20px"
-                        }}
-                    >
-                         <AppBar component="nav">
-                            <Typography fontSize={34} fontWeight={800}>
-                                Ranjit Marathay
-                            </Typography>
-                        </AppBar>
-                    </Box>
-                </Grid> */}
                 <Grid item xs={12}>
                     <Box
                         sx={{
@@ -113,10 +119,11 @@ export default function LandingPage(){
                     </Box>
                 </Grid>
                 <Grid item xs={12}>
+                {/* sm={6} md={4} lg={3}> */}
                     <Typography fontSize={28} fontWeight={600}>
                         Burdened by Manual Processes?
                     </Typography>
-                    <Box sx={{paddingLeft: "30px", paddingBottom: "25px"}}>
+                    <Box sx={{paddingLeft: paddingLeftVar, paddingBottom: "25px"}}>
                         <Box
                             sx={{
                                 textAlign: "left",
@@ -155,10 +162,11 @@ export default function LandingPage(){
                     </Box>
                 </Grid>
                 <Grid item xs={12}>
+                {/* sm={6} md={4} lg={3}> */}
                     <Typography fontSize={28} fontWeight={600}>
                         Navigating a New Acquisition?
                     </Typography>
-                    <Box sx={{paddingLeft: "30px", paddingBottom: "25px"}}>
+                    <Box sx={{paddingLeft: paddingLeftVar, paddingBottom: "25px"}}>
                         <Box
                             sx={{
                                 textAlign: "left",
@@ -188,10 +196,11 @@ export default function LandingPage(){
                     </Box>
                 </Grid>
                 <Grid item xs={12}>
+                {/* sm={6} md={4} lg={3}> */}
                     <Typography fontSize={28} fontWeight={600}>
                         Relying on Spreadsheets?
                     </Typography>
-                    <Box sx={{paddingLeft: "30px", paddingBottom: "25px"}}>
+                    <Box sx={{paddingLeft: paddingLeftVar, paddingBottom: "25px"}}>
                         <Box
                             sx={{
                                 textAlign: "left",
@@ -221,10 +230,11 @@ export default function LandingPage(){
                     </Box>
                 </Grid>
                 <Grid item xs={12}>
+                {/* sm={6} md={4} lg={3}> */}
                     <Typography fontSize={28} fontWeight={600}>
                         Worried about the cost of professional IT or software engineering services?
                     </Typography>
-                    <Box sx={{paddingLeft: "30px", paddingBottom: "25px"}}>
+                    <Box sx={{paddingLeft: paddingLeftVar, paddingBottom: "25px"}}>
                         <Box
                             sx={{
                                 textAlign: "left",
